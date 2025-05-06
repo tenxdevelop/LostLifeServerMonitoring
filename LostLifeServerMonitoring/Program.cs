11c293cdb4effbecb1642fdd67b2b5bbf4dc332b
Tenxdev <tenxdeveloper@bk.ru>
2025-05-06 22:55:00 +0500

    settings web project add basic pipeline

--- LostLifeServerMonitoring/Program.cs
+++ LostLifeServerMonitoring/Program.cs
@@ -1,7 +1,30 @@
-var builder = WebApplication.CreateBuilder(args);
+/**************************************************************************\
+    Copyright (C) 2024-2025 SkyForge Corporation. All Rights Reserved.
+    Author: Stepan Myasnikov --> tenxdeveloper.
+\**************************************************************************/
 
-var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
-app.Run();
+namespace LostLifeServerMonitoring
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = CreateHostBuilder(args);
+            
+            var host = builder.Build();
+            
+            host.Run();
+        }
+        
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var hostBuilder = Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder =>
+            { 
+                webBuilder.UseStartup<Startup>();
+            });
+            
+            return hostBuilder;
+        }
+    }
+    
+}
---
+++ LostLifeServerMonitoring/Startup.cs
@@ -0,0 +1,51 @@
+/**************************************************************************\
+    Copyright (C) 2024-2025 SkyForge Corporation. All Rights Reserved.
+    Author: Stepan Myasnikov --> tenxdeveloper.
+\**************************************************************************/
+
+namespace LostLifeServerMonitoring
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        
+        public void ConfigureServices(IServiceCollection services)
+        {
+            ServicesRegisterer.RegisterServices(services);
+            CommandRegisterer.RegisterCommandHandlers(services);
+            
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll", policy =>
+                {
+                    policy.AllowAnyHeader();
+                    policy.AllowAnyMethod();
+                    policy.AllowAnyOrigin();
+                });
+            });
+        }
+        
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            
+            app.UseRouting();
+            
+            app.UseHttpsRedirection();
+            app.UseCors("AllowAll");
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
